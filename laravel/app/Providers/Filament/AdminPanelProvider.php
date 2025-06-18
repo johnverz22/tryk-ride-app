2b7fe285a22056b627885e93c391cb7f1176f7ad
LDSPrgrm <ldsprgrm@gmail.com>
2025-06-18 09:51:06 +0800

    added user auth

--- laravel/app/Providers/Filament/AdminPanelProvider.php
+++ laravel/app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,8 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->passwordReset()
+            ->emailVerification()
             ->colors([
                 'primary' => Color::Green,
             ])
